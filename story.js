ccde9c3802c48a15f60517d4a57fc03bf6679f3f
aliigraibe <aliigraibe93@gmail.com>
2021-05-17 16:19:02 +0300

    Coding

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "Ali"
+let age = 27;
+let hobby = "ujikj"
+
+console.log(
+    `Mr./Mrs. ${name} in 33 years, you will be ${age+33} years old.
+     
+    By then youll have decades of ${hobby} practice`
+);
